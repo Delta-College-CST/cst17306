6585e17ef19b5fc6dcc96d593c83688981bfe0c4
teklingl <teklingl@gmail.com>
2024-08-06 14:28:58 -0400

    Study/Practice

--- nestedloop.py
+++ nestedloop.py
@@ -1,6 +1,6 @@
 # Demonstrates a basic nested set of for-loops
 
-for i in range (0,4):
-    for j in range (0,5):
+for i in range (1,4):
+    for j in range (1,5):
         print(i," ",j)
     print()
