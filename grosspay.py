9cbae661c9edea3edfc5cb8a24340662039b09ae
teklingl <teklingl@gmail.com>
2024-08-09 19:02:55 -0400

    Study/Practice

--- grosspay.py
+++ grosspay.py
@@ -1,6 +1,3 @@
-# This program receives hours worked and pay rate for an hourly
-# worker.  It calcualates gross pay based on standard overtime rules.
-
 BASE_HOURS = 40.0
   
 hours  = float(input("Enter hours worked: "))
@@ -9,6 +6,6 @@ rate   = float(input("Enter pay rate: "))
 if hours <= BASE_HOURS:
     pay = hours * rate
 else:
-    pay = hours * BASE_HOURS + (hours - BASE_HOURS) * 1.5 * rate
+    pay = rate * BASE_HOURS + (hours - BASE_HOURS) * 1.5 * rate
 
 print("For", hours, "hours, Pay = $%7.2f" % (pay))
