a243c9a58ce6ea3580ac3cdeaa4c663127ff070f
teklingl <teklingl@gmail.com>
2024-08-10 13:29:02 -0400

    Delete temptableF.py

--- temptableF.py
+++
@@ -1,7 +0,0 @@
-# General a table of Fahrenheit and Celsius temperatures
-
-print(" Fahrenheit Celsius ")
-for tempF in range(-40, 120, 5):
-    tempC = 5.0 / 9.0 * (tempF - 32.0)
-    print ("%7d  %7d" % (tempF, tempC))
-
